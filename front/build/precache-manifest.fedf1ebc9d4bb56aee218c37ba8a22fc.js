e461c7face45222f2120a8abc781fa8343ac586c
watarun <watarun54@gmail.com>
2018-10-18 16:11:58 +0900

    fix front design

---
+++ front/build/precache-manifest.fedf1ebc9d4bb56aee218c37ba8a22fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "56d7ef0bb0a4185dd825",
+    "url": "/static/js/main.56d7ef0b.chunk.js"
+  },
+  {
+    "revision": "f6c33828d64adfaf0b8e",
+    "url": "/static/js/1.f6c33828.chunk.js"
+  },
+  {
+    "revision": "56d7ef0bb0a4185dd825",
+    "url": "/static/css/main.ac601b29.chunk.css"
+  },
+  {
+    "revision": "f6c33828d64adfaf0b8e",
+    "url": "/static/css/1.60f5957f.chunk.css"
+  },
+  {
+    "revision": "55bc1c21994737b93c927c0f65f07f17",
+    "url": "/index.html"
+  }
+];
