a103860fc70c8c6c0216a4466c62b4067bbe5ae9
watarun <watarun54@gmail.com>
2018-10-21 15:35:21 +0900

    fix register.js

---
+++ api/routes/register.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var db = require('../db');
+var async = require('async');
+var router = express.Router();
+
+
+router.get('/', function(req, res, next) {
+    db.pool.query('SELECT * FROM users;', (err, results, fields)=>{
+        if (err) {
+          res.status(500).json({"error": err});
+        } else {
+          res.json({"users": results});
+        }
+    });
+});
+
+router.post('/', function(req, res, next) {
+    async.waterfall(
+        [
+          cb => {
+            let email = req.body.email;
+            let password = req.body.password;
+            if (email.length === 0 || password.length === 0) {
+              res.status(500).json({"error": "empty"});
+              cb(500);
+            } else {
+              cb(null, email, password);
+            }
+          },
+          (email, password, cb) => {
+            db.pool.query('SELECT * FROM users WHERE email =? LIMIT 1;',
+                [email], (err, results, fields) => {
+                    if(err) {
+                        res.status(500).json({"error": err});
+                        cb(500);
+                    } else if (results.length === 0) {
+                        cb(null, email, password);
+                    } else {
+                        res.status(500).json({"error": "exist"});
+                        cb(500);
+                    }
+                });
+          },
+          (email, password, cb) => {
+            db.pool.query('INSERT INTO users (email,password) VALUES (?,?);',
+                [email, password], (err, results, fields) => {
+                    if (err) {
+                        res.status(500).json({"error": err});
+                        cb(err);
+                    } else {
+                        res.json({
+                        "data":
+                            {
+                            "id": results.insertId,
+                            "email": email,
+                            "password": password,
+                            },
+                        "results": results
+                        });
+                        cb(null);
+                    }
+                });
+          }
+        ]
+      );
+});
+
+
+module.exports = router;
