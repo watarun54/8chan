589abf0df2ab7f9239263c9857036e1b6404aa23
watarun <watarun54@gmail.com>
2018-10-26 18:03:59 +0900

    fix posts table and feature usres table

--- mysql/sqls/init.sql
+++ mysql/sqls/init.sql
@@ -6,13 +6,14 @@ CREATE TABLE posts (
   `text` varchar(140) not null,
   `priority` int(12) not null,
   `name` varchar(140) default null,
+  `user_id` int(12) not null,
   `created_at` datetime not null default current_timestamp,
   `updated_at` datetime not null default current_timestamp on update current_timestamp,
   primary key (id)
 );
 
-INSERT INTO posts (text,priority,name) VALUES ("test1",0,"created");
-INSERT INTO posts (text,priority,name) VALUES ("テスト1",0,"created");
+INSERT INTO posts (text,priority,name,user_id) VALUES ("test1",0,"created",1);
+INSERT INTO posts (text,priority,name,user_id) VALUES ("テスト1",0,"created",2);
 
 CREATE TABLE users (
   `id` int(12) unsigned not null auto_increment,
@@ -24,3 +25,5 @@ CREATE TABLE users (
 );
 
 INSERT INTO users (email,password) VALUES ("test1@example.com","password");
+INSERT INTO users (email,password) VALUES ("test2@example.com","password");
+
